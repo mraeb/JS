50e4aba6ad4cd54cac98fa32fed36261b792b1d6
mraeb <abhishekbalajisaravanan@gmail.com>
2026-02-07 13:42:13 +0530

    js file and index.html file added

--- sum2nums.js
+++ sum2nums.js
@@ -1,6 +1,9 @@
-let num1 = prompt("Enter the first number: ");
-let num2 = prompt("Enter the second number: ");
+document.getElementById("title").innerHTML = "Sum Of Two Numbers";
+let num1 = Number(prompt("Enter the first number: ") || 0);
+let num2 = Number(prompt("Enter the second number: ") || 0);
+// let sum = Number(num1)+Number(num2);
+let sum = num1 + num2;
+console.log("Sum of two numbers: " + sum);
+document.getElementById("result").innerHTML = "Sum of two numbers: " + sum;
 
-let sum = Number(num1)+Number(num2);
-
-console.log("Sum of two numbers: "+ sum);
+document.getElementById("result").innerHTML = "Sum: " + sum;
